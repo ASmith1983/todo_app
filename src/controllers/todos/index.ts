8af49ed576a15902fcf9720744ff06641712a6d3
Andrew Smith <asmith@DrewLaptop.localdomain>
2022-06-21 10:58:38 -0700

    CRUD complete on controllers/todos/index.ts

--- src/controllers/todos/index.ts
+++ src/controllers/todos/index.ts
@@ -5,6 +5,8 @@ import { Response, Request } from "express";
 import { ITodo } from "../../types/todo";
 import Todo from "../../models/todo";
 
+// Setting up CRUD.. note about promise<void> it may be used to create future<void> objects which have no value. In addition, a promise<void> can be constructed from a promise<T> where T is any type. This allows a promise<void> to be used by code which only needs to be able to renege on a promise and not fulfill it.
+
 const getTodos =async (req: Request, res: Response): Promise<void> => {
    try{
     const todos: ITodo[] = await Todo.find()
@@ -34,3 +36,42 @@ const addTodo =async (req: Request, res: Response): Promise<void> => {
     throw error
    }
 }
+
+const updateTodo =async (req:Request, res: Response): Promise<void> => {
+   try{
+    const{ params: { id }, body, } = req
+    const updateTodo: ITodo | null = await Todo.findByIdAndUpdate(
+        { _id: id },
+        body,
+    )
+    const allTodos: ITodo[] = await Todo.find()
+    res.status(200).json({
+        message: "Todo updated",
+        todo: updateTodo,
+        todos: allTodos,
+    })
+   } catch (error){
+    throw error
+   }
+}
+
+const deleteTodo =async (req:Request, res: Response): Promise<void> => {
+   try{
+
+    const deletedTodo: ITodo | null = await Todo.findByIdAndRemove(
+        req.params.id
+    )
+    const allTodos: ITodo[] = await Todo.find()
+    res.status(200).json({
+        message: "Todo deleted",
+        todo: deletedTodo,
+        todos: allTodos,
+    })
+   } catch (error){
+    throw error
+   }
+
+   } 
+}
+
+export { getTodos, addTodo, updateTodo, deleteTodo }
